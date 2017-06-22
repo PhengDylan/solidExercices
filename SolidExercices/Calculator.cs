a697c6fa1a4e6a9cbd22750d591677f455cfad6b
PhengDylan <ph3ng.dylan@gmail.com>
2017-06-22 15:03:04 +0200

    Ajout de la première fonction d'addition avec premier Test qui passe

--- SolidExercices/Calculator.cs
+++ SolidExercices/Calculator.cs
@@ -7,7 +7,30 @@ namespace SolidExercices
     {
         public double Calculate(string operation)
         {
-            throw new NotImplementedException();
+
+            double resultat = 0;
+            //On enlève tout les espaces possible dans le String
+            var operationTrim = operation.Trim();
+
+
+            //////////////////*          Gestion d'une addition              *//////////////////
+           if (operationTrim.Contains('+') == true )
+            { 
+                // 1 -Il faut séparer les opérations des nombres
+                var tabString = operationTrim.Split('+');
+                // 2- Convertir les différentes chaîne en type double et faire le calcule
+                for (int i = 0; i < tabString.Length; i++)
+                {
+                    resultat = resultat + Convert.ToDouble(tabString[i]);
+                }
+           }
+           
+            return resultat;
+            // throw new NotImplementedException();
         }
+
+        
     }
+
+    
 }
